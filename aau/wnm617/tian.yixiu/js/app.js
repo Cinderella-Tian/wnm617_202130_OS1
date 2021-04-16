06a5de49622f9b3a0ee2b190f9dd3e08be7a3583
Cinderella-Tian <ningxueyangraduate@gmail.com>
2021-04-16 12:58:36 -0700

    Update app.js

--- aau/wnm617/tian.yixiu/js/app.js
+++ aau/wnm617/tian.yixiu/js/app.js
@@ -1,70 +1,85 @@
 // Document Ready
-
 $(()=>{
 
-	checkUserId();
+   checkUserId();
 
-	$(document)
-
-
-    /* Form Submits */
-	.on("submit","#signin-form",function(e){
-		e.preventDefault();
-		checkSigninForm();
-	})
+   $(document)
 
 
+   .on("pagecontainerbeforeshow", function(event, ui) {
+      console.log(ui.toPage[0].id)
+
+      // PAGE ROUTING
+      switch(ui.toPage[0].id) {
+         case "recent-page": RecentPage(); break;
+         case "list-page": ListPage(); break;
+         case "user-profile-page": UserProfilePage(); break;
+         case "animal-profile-page": AnimalProfilePage(); break;
+      }
+   })
 
 
+   /* FORM SUBMITS */
+   .on("submit","#signin-form",function(e){
+      e.preventDefault();
+      checkSigninForm();
+   })
 
 
-	/* Anchor Clicks */
-	.on("click",".js-logout",function(e){
-		sessionStorage.removeItem('userId');
-		checkUserId();
-	})
+   /* ANCHOR CLICKS */
+   .on("click",".js-logout",function(e){
+      sessionStorage.removeItem('userId');
+      checkUserId();
+   })
+   .on("click",".animal-jump",function(e){
+      sessionStorage.animalId = $(this).data('id');
+      $.mobile.navigate("#animal-profile-page")
+   })
+   .on("click",".animal-nav a",function(e){
+      let id = $(this).parent().index();
+      
+      $(this).parent().addClass("active")
+         .siblings().removeClass("active")
 
-
-
-
-
-
-
-
-
-	/* Data Activate */
-	.on("click","[data-activate]",function(e){
-		let target = $(this).data("activate");
-		$(target).addClass("active");
-	})
-
-	.on("click","[data-deactivate]",function(e){
-		let target = $(this).data("deactivate");
-		$(target).removeClass("active");
-	})
-
-	.on("click","[data-toggle]",function(e){
-		let target = $(this).data("toggle");
-		$(target).toggleClass("active");
-	})
-
-
+      $(this)
+         .closest(".animal-nav").next().children().eq(id)
+         .addClass("active")
+         .siblings().removeClass("active")
+   })
 
 
 
+   /* DATA ACTIVATE */
+   .on("click","[data-activate]",function(e){
+      let target = $(this).data("activate");
+      $(target).addClass("active");
+   })
+   .on("click","[data-deactivate]",function(e){
+      let target = $(this).data("deactivate");
+      $(target).removeClass("active");
+   })
+   .on("click","[data-toggle]",function(e){
+      let target = $(this).data("toggle");
+      $(target).toggleClass("active");
+   })
 
 
 
 
 
 
-	$("[data-template]").each(function(){
-		let id = $(this).data("template");
-		let template = $(id).html();
-		$(this).html(template);
-	})
+
+
+
+   $("[data-template]").each(function(){
+      let id = $(this).data("template");
+      let template = $(id).html();
+      $(this).html(template);
+   })
 
 });
+
+
 
 
 
