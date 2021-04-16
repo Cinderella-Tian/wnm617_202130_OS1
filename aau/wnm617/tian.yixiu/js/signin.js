571bf2c7e1dff81b778d0af6692338ca11512861
Cinderella-Tian <ningxueyangraduate@gmail.com>
2021-04-16 12:58:44 -0700

    Update signin.js

--- aau/wnm617/tian.yixiu/js/signin.js
+++ aau/wnm617/tian.yixiu/js/signin.js
@@ -1,33 +1,46 @@
 
-const checkSigninForm = () => {
-	let username = $("#signin-username").val();
-	let password = $("#signin-password").val();
+// Check and change data
+const checkSigninForm = async () => {
+   let username = $("#signin-username").val();
+   let password = $("#signin-password").val();
 
-	if(username=="user" && password=="pass") {
-		console.log("logged in")
-		sessionStorage.userId = 3;
-	} else {
-		console.log("logged out")
-		sessionStorage.removeItem("userId");
-	}
+   if(username=='' || password=='') {
+      // warn that not all information is there
+      return;
+   }
 
-	checkUserId();
+   let user = await query({
+      type:'check_signin',
+      params:[username,password]
+   });
+
+   if(user.result.length > 0) {
+      console.log("logged in")
+      sessionStorage.userId = user.result[0].id;
+
+      $("#signin-form")[0].reset();
+   } else {
+      console.log("logged out")
+      sessionStorage.removeItem("userId");
+   }
+
+   checkUserId();
 }
 
 
+// Separation of concerns - seperating out every part of application if is possible
 
+// Change page base on data
 const checkUserId = () => {
-	let p = ["#signin-page","#signup-page",""];
+   let p = ["#signin-page","#signup-page",""];
 
-	if(sessionStorage.userId === undefined) {
-		// not logged in
-		if(!p.some(o=>window.location.hash===o))
-			$.mobile.navigate("#signin-page")
-
-	} else {
-		// logged in
-		if(p.some(o=>window.location.hash===o))
-			$.mobile.navigate("#recent-page")
-
-	}
+   if(sessionStorage.userId === undefined) {
+      // not logged in
+      if(!p.some(o=>window.location.hash===o))
+         $.mobile.navigate("#signin-page");
+   } else {
+      // logged in
+      if(p.some(o=>window.location.hash===o))
+         $.mobile.navigate("#recent-page");
+   }
 }
